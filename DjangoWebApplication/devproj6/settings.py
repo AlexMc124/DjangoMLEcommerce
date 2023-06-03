88e30be813ce1b2a8ce4664d6efc978d29d8e431
Alex McIntosh <61054816+AlexMc124@users.noreply.github.com>
2023-06-03 15:57:00 +0100

    Update settings.py

--- DjangoWebApplication/devproj6/settings.py
+++ DjangoWebApplication/devproj6/settings.py
@@ -126,11 +126,6 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 LOGIN_REDIRECT_URL = 'store-home'
 LOGIN_URL = 'login'
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'alex.mcintosh124@gmail.com'
-EMAIL_HOST_PASSWORD = 'B00b$124'
+
 #export DJANGO_SETTINGS_MODULE=mysite.settings
 
